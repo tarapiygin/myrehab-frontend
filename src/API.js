f62b666364ffe92a88461f0d9cc1a855ddd09611
Denis Vassilyev <tarapiygin@gmail.com>
2022-04-09 19:10:44 +0300

    рефакторинг кода

--- src/API.js
+++ src/API.js
@@ -1,86 +1,62 @@
-import Notice from './Notice';
-import {HREF, ORIGIN} from './app';
-import {getCookie} from './utils';
+import { getCookie } from './utils';
+
+export const HREF = 'http://127.0.0.1/';
+export const ORIGIN = 'http://127.0.0.1';
 
 export default class API {
-  static async getResponse(response) {
+  static async getResponse(adress, method = 'GET', body = undefined, exHeaders = {}) {
+    const url = new URL(ORIGIN + adress);
+    const headers = exHeaders;
+    if (method in ['POST', 'PUT', 'DELETE']) {
+      headers['X-CSRFToken'] = getCookie();
+    }
+    const response = await fetch(url.href, { headers, method, body });
     try {
       if (response.ok) {
         // если HTTP-статус в диапазоне 200-299
         return await response.json();
       }
-      Notice.show('Возникла ошибка при отправке данных', false);
+      return { status: 'error', message: response.error };
     } catch (error) {
-      throw new Error(error.message);
+      return { status: 'error', message: error };
     }
   }
 
   static async getUserData() {
     const adress = '/reestr/api/user';
-    const url = new URL(ORIGIN + adress);
-    const response = await API.getResponse(await fetch(url.href));
-    if (response.status === 'ok') return response.data;
-    Notice.show('Возникла ошибка при получении данных пользователя =(');
+    return API.getResponse(adress);
   }
 
   static async getSpecialties() {
     const adress = '/reestr/api/specialty';
-    const url = new URL(ORIGIN + adress);
-    const response = await API.getResponse(await fetch(url.href));
-    if (response.status === 'ok') return response.data.specialties;
-    Notice.show('Возникла ошибка при получении данных специальностей =(');
-  }
-
-  static async getSpecialtyById(id) {
-    const adress = '/reestr/api/specialty';
-    const url = new URL(ORIGIN + adress);
-    url.searchParams.set('id', id);
-    const response = await API.getResponse(await fetch(url.href));
-    if (response.status === 'ok') return response.data;
-    Notice.show('Возикла ошибка при получении данных специальности =(');
+    return API.getResponse(adress);
   }
 
   static async updateUser(body) {
     const adress = '/reestr/api/user';
-    const url = new URL(ORIGIN + adress);
-    // отправляем запрос на создание пациента, если все ОК, то ничего не вернется
-    // если ошибка, то отрисовываем их в виджете
     const method = 'PUT';
-    const headers = {
-      "X-CSRFToken": getCookie()
-    }
-    return await API.getResponse(await fetch(url.href, {headers: headers, method, body}));
+    return API.getResponse(adress, method, body);
   }
 
   static async updateMeeting(body) {
     const adress = '/reestr/api/meeting';
-    const url = new URL(ORIGIN + adress);
     const method = 'PUT';
-    const headers = {
-      "X-CSRFToken": getCookie()
-    }
-    return await API.getResponse(await fetch(url.href, {headers: headers, method, body}));
+    return API.getResponse(adress, method, body);
   }
 
   static async updateStudent(body) {
     const adress = '/reestr/api/student';
-    const url = new URL(ORIGIN + adress);
-    // отправляем запрос на создание пациента, если все ОК, то ничего не вернется
-    // если ошибка, то отрисовываем их в виджете
     const method = 'PUT';
-    const headers = {
-      "X-CSRFToken": getCookie()
-    }
-    return await API.getResponse(await fetch(url.href, {headers: headers, method, body}));
+    return API.getResponse(adress, method, body);
   }
 
-
-  static async createPatient(body) {
-    // отправляем запрос на создание пациента, если все ОК, то ничего не вернется
-    // если ошибка, то отрисовываем их в виджете
-    const method = 'POST';
-    return await API.getResponse(await fetch(HREF, {method, body}));
-  }
+  // static async createPatient(body) {
+  //   // отправляем запрос на создание пациента, если все ОК, то ничего не вернется
+  //   // если ошибка, то отрисовываем их в виджете
+  //   const adress = '';
+  //   const method = 'POST';
+  //   return API.getResponse(adress, method, body);
+  // }
 
   static async getDataSearchForm() {
     const url = new URL(HREF);
