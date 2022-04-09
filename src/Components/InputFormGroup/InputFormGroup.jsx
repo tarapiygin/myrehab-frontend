0c27432992487e4ec0402a2877051c27e78b7951
Denis Vassilyev <tarapiygin@gmail.com>
2022-04-09 19:15:36 +0300

    Добавлен компонент Input

---
+++ src/Components/InputFormGroup/InputFormGroup.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+export default function InputFormGroup({
+  name, label, initValue = '', requred = true, type = 'text', extendedClass = 'col-12 col-sm-6 col-md-4',
+}) {
+  const [value, setValue] = useState(initValue);
+  return (
+  <div className={`${extendedClass} form-group`}>
+    <label htmlFor={name}>{label}</label>
+    <input required={requred} type={type} className="form-control" placeholder={label} name={name} defaultValue={value}/>
+  </div>
+  );
+}
