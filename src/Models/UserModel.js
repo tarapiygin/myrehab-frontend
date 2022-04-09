fc4b9c2ee348d942b2d34851ee31758dedacae5d
Denis Vassilyev <tarapiygin@gmail.com>
2022-04-09 19:16:05 +0300

    Добавлена базовая модель пользователя

--- src/Models/UserModel.js
+++ src/Models/UserModel.js
@@ -1,20 +1,22 @@
 import PropTypes from 'prop-types';
+import StudentModel from './StudentModel';
+import PatientModel from './PatientModel';
 
 const UserModel = {
-  id: PropTypes.number.isRequired,
-  email: PropTypes.string.isRequired,
-  first_name: PropTypes.string.isRequired,
+  id: PropTypes.number,
+  email: PropTypes.string,
+  first_name: PropTypes.string,
   last_name: PropTypes.string,
   middle_name: PropTypes.string,
   phone: PropTypes.string,
   country: PropTypes.string,
   city: PropTypes.string,
   avatar: PropTypes.string,
-  date_joined: PropTypes.string.isRequired,
+  date_joined: PropTypes.string,
   last_login: PropTypes.string,
-  is_superuser: PropTypes.bool.isRequired,
-  is_staff: PropTypes.bool.isRequired,
-  is_active: PropTypes.bool.isRequired,
+  is_superuser: PropTypes.bool,
+  is_staff: PropTypes.bool,
+  is_active: PropTypes.bool,
   student: PropTypes.object,
   patient: PropTypes.object,
 };
