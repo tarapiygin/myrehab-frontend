23830a9e323e4cfb150bac8c50cd9c8904971798
Denis Vassilyev <tarapiygin@gmail.com>
2022-04-10 22:26:15 +0300

    сделано меню

--- src/Components/HomePageMenu/HomePageMenu.jsx
+++ src/Components/HomePageMenu/HomePageMenu.jsx
@@ -1,12 +1,24 @@
-export default function HomePageMenu(props) {
-  const type = props;
-  console.log(type);
+import { useRef, useState } from 'react';
+import './HomePageMenu.css';
+
+export default function HomePageMenu({ student, patient, onToggleComponent }) {
+  const onClickButton = (e) => {
+    onToggleComponent(e.target.dataset.type);
+    const buttons = refContainer.current.querySelectorAll('.HomePageMenu__toggle');
+    for (const b of buttons) {
+      b.classList.remove('HomePageMenu__toggle--active');
+      if (b.dataset.type === e.target.dataset.type) b.classList.add('HomePageMenu__toggle--active');
+    }
+  };
+
+  const refContainer = useRef();
+
   return (
-    <div className="HomePageMenu">
-      <button className="btn HomePageMenu-button" type="button">Личный профиль</button>
-      {console.log(type) || type === 'student' || <button className="btn HomePageMenu-button" type="button">Профиль специалиста</button>}
-      <button className="btn HomePageMenu-button" type="button">Записи к специалисту</button>
-      {type === 'student' || <button className="btn HomePageMenu-button" type="button">Мои пациенты</button>}
+    <div className="HomePageMenu" ref={refContainer}>
+      <button onClick={onClickButton} data-type='user' className="btn HomePageMenu__toggle HomePageMenu__toggle--active" type="button">Личный профиль</button>
+      {student && <button onClick={onClickButton} data-type='student' className="btn HomePageMenu__toggle" type="button">Профиль специалиста</button>}
+      {student && <button onClick={onClickButton} data-type='studentMeetings' className="btn HomePageMenu__toggle" type="button">Мои пациенты</button>}
+      {patient && <button onClick={onClickButton} data-type='patientMeetings' className="btn HomePageMenu__toggle" type="button">Записи к специалисту</button>}
     </div>
   );
 }
