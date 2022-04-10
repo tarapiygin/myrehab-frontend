d501b494a3aa1d72d4f5637f1ff68b788b220940
Denis Vassilyev <tarapiygin@gmail.com>
2022-04-10 21:06:40 +0300

    Добавлена очередь запросов и их отмена при повторе

--- src/API.js
+++ src/API.js
@@ -2,15 +2,33 @@ import { getCookie } from './utils';
 
 export const HREF = 'http://127.0.0.1/';
 export const ORIGIN = 'http://127.0.0.1';
-
+const CALLS = [];
 export default class API {
   static async getResponse(adress, method = 'GET', body = undefined, exHeaders = {}) {
     const url = new URL(ORIGIN + adress);
     const headers = exHeaders;
+    const controller = new AbortController();
+
     if (method in ['POST', 'PUT', 'DELETE']) {
       headers['X-CSRFToken'] = getCookie();
     }
-    const response = await fetch(url.href, { headers, method, body });
+    const i = CALLS.findIndex((c) => c.href === url.href && c.method === method);
+    if (i !== -1) {
+      CALLS[i].controller.abort();
+      CALLS.splice(i, 1);
+    }
+    const call = {
+      href: url.href,
+      headers,
+      method,
+      body,
+      controller,
+    };
+    CALLS.push(call);
+    const response = await fetch(url.href, {
+      headers, method, body, signal: controller.signal,
+    });
+
     try {
       if (response.ok) {
         // если HTTP-статус в диапазоне 200-299
@@ -27,9 +45,39 @@ export default class API {
     return API.getResponse(adress);
   }
 
+  static async getStudentData() {
+    const adress = '/reestr/api/student';
+    return API.getResponse(adress);
+  }
+
+  static async getPatientData() {
+    const adress = '/reestr/api/patient';
+    return API.getResponse(adress);
+  }
+
+  static async getMeetingsData() {
+    const adress = '/reestr/api/meeting';
+    return API.getResponse(adress);
+  }
+
   static async getSpecialties() {
     const adress = '/reestr/api/specialty';
     return API.getResponse(adress);
+  }
+
+  static async getAlldata() {
+    const user = await API.getUserData();
+    const student = await API.getStudentData();
+    const patient = await API.getPatientData();
+    const meetings = await API.getMeetingsData();
+    const state = {
+      user: user.data,
+      student: student.data,
+      patient: patient.data,
+      studentMeetings: meetings.data.student_meetings,
+      patientMeetings: meetings.data.patient_meetings,
+    };
+    return state;
   }
 
   static async updateUser(body) {
