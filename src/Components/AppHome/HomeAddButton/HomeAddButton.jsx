44f8ea80b322b8b56cbb105d84eee6f49c553ca6
Denis Vassilyev <tarapiygin@gmail.com>
2022-04-26 18:43:42 +0300

    styles refactoring

--- src/Components/AppHome/HomeAddButton/HomeAddButton.jsx
+++ src/Components/AppHome/HomeAddButton/HomeAddButton.jsx
@@ -1,9 +1,9 @@
-import './HomeAddButton.css';
+import styles from './HomeAddButton.module.css';
 
 export default function HomeAddButton({ onClick, text }) {
   return (
-  <div className='HomeAddButtonContainer'>
-    <button onClick={onClick} className='Kalend__button Kalend__ButtonBase-border Kalend__ButtonBase HomeAddButton'>{text}</button>
+  <div className={styles.container}>
+    <button onClick={onClick} className={`Kalend__button Kalend__ButtonBase-border Kalend__ButtonBase ${styles.button}`}>{text}</button>
   </div>
   );
 }
